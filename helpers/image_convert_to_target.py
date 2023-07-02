5c549910f7ab5d19978a71c62b7b633fde81b614
Andres Espitia <espitiaandres123@gmail.com>
2023-07-02 17:07:58 -0400

    add docstrings

--- helpers/image_convert_to_target.py
+++ helpers/image_convert_to_target.py
@@ -8,6 +8,13 @@ from wand.image import Image
 
 @penguin(foreground="blue")
 def image_convert_to_target(file_name: str):
+    """
+    Convert a given image to the specified output file type in constants.py
+
+    Args:
+        file_name (str): Name of the file to convert
+    """
+
     source_file = f"{constants.INPUT_PATH}/{file_name}"
 
     img = Image(filename=source_file)
@@ -23,4 +30,5 @@ def image_convert_to_target(file_name: str):
     full_new_file_name = os.path.join(constants.OUTPUT_PATH, new_file_name)
     img.save(filename=full_new_file_name)
     img.close()
+
     return
--- helpers/image_move.py
+++ helpers/image_move.py
@@ -5,8 +5,17 @@ from penguin_py import penguin
 
 
 @penguin(foreground="yellow")
-def image_copy(file_name):
+def image_copy(file_name: str):
+    """
+    For files that are already of the specified output file type, just move these from 
+    the INPUT_PATH to the OUTPUT_PATH
+
+    Args:
+        file_name (str): _description_
+    """
+
     source_file = f"{c.INPUT_PATH}/{file_name}"
     target_file = f"{c.OUTPUT_PATH}/{file_name}"
     shutil.copyfile(source_file, target_file)
+
     return
